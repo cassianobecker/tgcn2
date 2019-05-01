32a61564da779d810263cbc3265e7b04d2cd1a0f
semihcanturk <canturks@seas.upenn.edu>
2019-05-01 19:08:22 +0300

    Added documentation for hcp_gcn.py (Issue #6)

--- experiments/hcp/classifier/hcp_gcn.py
+++ experiments/hcp/classifier/hcp_gcn.py
@@ -17,7 +17,10 @@ from nn.chebnet import ChebTimeConv
 
 
 class NetTGCNBasic(torch.nn.Module):
-
+    """
+    A 1-Layer time graph convolutional network
+    :param mat_size: temporary parameter to fix the FC1 size
+    """
     def __init__(self, mat_size):
         super(NetTGCNBasic, self).__init__()
 
@@ -32,10 +35,21 @@ class NetTGCNBasic(torch.nn.Module):
         self.perm = None
 
     def add_graph(self, coos, perm):
+        """
+        Sets the COO adjacency matrix (or matrices post-coarsening) for the graph and the order of vertices in the matrix
+        :param coos: list of adjacency matrices for the graph
+        :param perm: order of vertices in the adjacency matrix
+        :return: None
+        """
         self.coos = coos
         self.perm = perm
 
     def forward(self, x):
+        """
+        Computes forward pass through the time graph convolutional network
+        :param x: windowed BOLD signal to as input to the TGCN
+        :return: output of the TGCN forward pass
+        """
         x, edge_index = x, self.coos[0]
 
         x = self.conv1(x, edge_index)
@@ -49,6 +63,19 @@ class NetTGCNBasic(torch.nn.Module):
 
 
 def train_minibatch(args, model, device, train_loader, optimizer, epoch, mini_batch=10, verbose=False):
+    """
+    Loads input data (BOLD signal windows and corresponding target motor tasks) from one patient at a time,
+    and minibatches the windowed input signal while training the TGCN by optimizing for minimal training loss.
+    :param args: keyword arguments (see main())
+    :param model: PyTorch Module/DataParallel object to model
+    :param device: device to send the data to
+    :param train_loader: DataLoader that hosts the training data
+    :param optimizer: optimizing algorithm (default=Adam)
+    :param epoch: current epoch
+    :param mini_batch: number of minibatches to go through before taking an optimizer step
+    :param verbose: boolean to print out training progress
+    :return: train_loss
+    """
     train_loss = 0
     model.train()
 
@@ -92,7 +119,16 @@ def train_minibatch(args, model, device, train_loader, optimizer, epoch, mini_ba
     return train_loss
 
 
-def test(args, model, device, test_loader, t1, epoch):
+def test(args, model, device, test_loader, epoch):
+    """
+    Evaluates the model trained in train_minibatch() on patients loaded from the test set.
+    :param args: keyword arguments (see main())
+    :param model: PyTorch Module/DataParallel object to model
+    :param device: device to send the data to
+    :param test_loader: DataLoader that hosts the test data
+    :param epoch: current epoch
+    :return: test_loss and correct, the # of correct predictions
+    """
     model.eval()
 
     test_loss = 0
@@ -131,6 +167,12 @@ def test(args, model, device, test_loader, t1, epoch):
 
 
 def experiment(args):
+    """
+    Sets up the experiment environment (loggers, data loaders, model, optimizer and scheduler) and initiates the
+    train/test process for the model.
+    :param args: keyword arguments from main() as parameters for the experiment
+    :return: None
+    """
     settings = get_settings()
     imageglobals.logger = set_logger('Nibabel', settings['LOGGING']['nibabel_logging_level'])
 
@@ -160,14 +202,13 @@ def experiment(args):
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
 
     for epoch in range(1, args.epochs + 1):
-        t1 = time.time()
 
         train_loss = train_minibatch(args, model, device, train_loader, optimizer, epoch, mini_batch=batch_size,
                                      verbose=True)
 
         scheduler.step()
 
-        test_loss, correct = test(args, model, device, test_loader, t1, epoch)
+        test_loss, correct = test(args, model, device, test_loader, epoch)
 
         print('Epoch: {} Training loss: {:1.3e}, Test loss: {:1.3e}, Accuracy: {}/{} ({:.2f}%)'.format(
             epoch, train_loss, test_loss, correct, len(test_loader.dataset) * 270,
@@ -178,6 +219,11 @@ def experiment(args):
 
 
 def seed_everything(seed=1234):
+    """
+    Sets a random seed for OS, NumPy, PyTorch and CUDA.
+    :param seed: random seed to apply
+    :return: None
+    """
     random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
