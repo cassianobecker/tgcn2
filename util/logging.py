41ec414b527de9f6e2de8f8195aa00aff2e7a7c8
semihcanturk <canturks@seas.upenn.edu>
2019-05-04 19:15:39 +0300

    formatting fix for logger

--- util/logging.py
+++ util/logging.py
@@ -8,6 +8,7 @@ def set_logger(name, level):
     :param level: Level of logger
     :return: logger, the configured Logger object
     """
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(name)
     level_dict = {
         'debug': logging.DEBUG,
@@ -18,8 +19,6 @@ def set_logger(name, level):
     }
     logger.setLevel(level_dict[level])
     log_stream = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     log_stream.setLevel(level_dict[level])
-    log_stream.setFormatter(formatter)
     logger.addHandler(log_stream)
     return logger
